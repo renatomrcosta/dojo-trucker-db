00ed8d6c81f1483a6019e6fbc5cf69dbbdf6e700
Renato <renato.rezendecosta@otto.de>
2021-06-10 14:44:30 +0200

    initial commit

---
+++ src/main/kotlin/com/xunfosechunfos/dojotruckerdb/DojoTruckerDbApplication.kt
@@ -0,0 +1,11 @@
+package com.xunfosechunfos.dojotruckerdb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DojoTruckerDbApplication
+
+fun main(args: Array<String>) {
+    runApplication<DojoTruckerDbApplication>(*args)
+}
---
+++ src/test/kotlin/com/xunfosechunfos/dojotruckerdb/DojoTruckerDbApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.xunfosechunfos.dojotruckerdb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DojoTruckerDbApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
